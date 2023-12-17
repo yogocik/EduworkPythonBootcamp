c4b1e5b8c53dd93d82000ebaf9a352d95788bfb4
Askarroffi <unknownperson6174@gmail.com>
2023-12-17 15:44:04 +0700

    data types askar

---
+++ Askar/python data types askar.py
@@ -0,0 +1,10 @@
+
+number : int = 20
+number_2 : float = 15.5
+boolean_var = True
+string_var = "my string"
+
+if __name__ == '__main__':
+    print(number + number_2)
+    print(number + boolean_var)
+    print(number + string_var)
