59712e1865ab06438dde1226274bfa6b54d01c6c
Abizar Adi <Abizar861@gmail.com>
2023-12-20 00:44:25 +0700

    Data type

---
+++ Abizar/Datatypes.py
@@ -0,0 +1,10 @@
+
+number : int = 12
+number_2 : float = 30.5
+boolean_var = True
+string_var = "My String"
+
+if __name__ == '_Main_':
+    print(number + number_2)
+    print(number + boolean_var)
+    print(number + string_var)
