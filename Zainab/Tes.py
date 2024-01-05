cedbfc318bd3e0ba4292d2574713ffebf4a2862f
Zaenab <Zaenab@DESKTOP-FOGI0I3.(none)>
2024-01-05 20:55:22 +0700

    create new folder

---
+++ Zainab/Tes.py
@@ -0,0 +1,13 @@
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halo</title>
+</head>
+<body>
+    Ini folder zainab
+</body>
+</html>
---
+++ Zainab/mainn.py
@@ -0,0 +1,27 @@
+# Numeric
+#my_number: int = "10"
+#print (my_number)
+#my_number_2:float = 1.6278
+#print ("My Float Number ->", my_number_2)
+# Boolean
+#is_studying = False
+#print ("My boolean Number ->", is_studying)
+# Nonetype
+#my_score = None
+#print ("My Score ->", my_score)
+# List
+#my_list = [1,2,3,True,3.1,"String"]
+#print (my_list[0:4])
+#my_list_2 = list ()
+#my_list.append([100,200])
+#print (my_list_2)
+# Casting
+#my_number = "10"
+#print ("My Number in Integer ->", my_number, type(my_number))
+#my_number_in_string = str(my_number)
+#print ("My Number in String ->", my_number_in_string, type(my_number_in_string))
+
+def sum_number(num_1, num_2) -> int:
+    return num_1 + num_2
+
+print (sum_number(1,2))
