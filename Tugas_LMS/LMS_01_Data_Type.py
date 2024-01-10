33b2606950288ba996b8ffb8ccc3c7a2be251b19
afri siringo <siringoafri@gmail.com>
2024-01-11 00:57:52 +0700

    pengerjaan tugas LMS Data Type

---
+++ Tugas_LMS/LMS_01_Data_Type.py
@@ -0,0 +1,14 @@
+number : int = 25
+angka : float = 2.5
+myBoolean = True
+myString = "STR"
+
+if __name__ == '__main__':
+    print(number + angka)
+    print(number + myBoolean)  # value var myBoolean akan otomatis diubah jadi 1
+    # print (number + myString)
+'''
+print(number + myString)
+
+Output : Error  -->int dan str tidak bisa lakukan operasi tambah
+'''
